3a78cc6ef053c54416f3a13bd95cbc6c9a603e05
JorgeLuisJP <u202021791@upc.edu.pe>
2023-04-08 18:35:27 -0500

    nuevos

--- app/src/main/java/com/example/trabajogrupal1/SesionActivity.java
+++ app/src/main/java/com/example/trabajogrupal1/SesionActivity.java
@@ -2,7 +2,9 @@ package com.example.trabajogrupal1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class SesionActivity extends AppCompatActivity {
 
@@ -11,4 +13,7 @@ public class SesionActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sesion);
     }
+
+    public void volverI(View view){startActivity(new Intent(this, RegistrosActivity.class));}
+
 }
